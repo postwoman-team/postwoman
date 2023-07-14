8eb91d288f064797e6ab1ad8fa38fffa8449439d
Hikari <hikaridesuyoo@gmail.com>
2023-07-14 17:15:01 -0300

    fixes program crashing at start because of no base found

--- utils/dependencies.rb
+++ utils/dependencies.rb
@@ -3,11 +3,21 @@ require 'bundler/setup'
 require_relative '../utils/loaders/builtin/base'
 Bundler.require
 
+def create_loader_base_unless_exists
+  path = 'loaders/base.rb'
+  return if File.exist?(path)
+
+  File.open(path, 'w') do |f|
+    f.write(File.read('templates/loader_base.rb'))
+  end
+end
+
 def load_loaders
   load 'loaders/base.rb'
   Dir[File.dirname(__FILE__) + '/../loaders/**/*.rb'].each { |file| load file }
 end
 
+create_loader_base_unless_exists
 Dir[File.dirname(__FILE__) + '/../utils/**/*.rb'].each { |file| require_relative file }
 load_loaders
 ENV.merge!(Dotenv.load)
