3c1c5e5a56ae70b6eb0e21e710bb3d116d826548
Hikari <hikaridesuyoo@gmail.com>
2023-07-18 17:30:39 -0300

    joins constant fetches under constant_names file

--- utils/command_names.rb
+++
@@ -1,3 +0,0 @@
-def fetch_command_names
-  (Commands.constants - [:Base]).map(&:to_s)
-end
--- utils/constant_names.rb
+++ utils/constant_names.rb
@@ -1,3 +1,7 @@
+def fetch_command_names
+  (Commands.constants - [:Base]).map(&:to_s)
+end
+
 def fetch_loader_names
   (Loaders.constants - %i[Builtin Utils]).map(&:to_s)
 end
