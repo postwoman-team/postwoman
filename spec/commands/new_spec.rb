4fcf19e9d01d6c63b21014f70426786fa4bb2e96
Hikari <hikaridesuyoo@gmail.com>
2023-07-26 13:29:58 -0300

    replaces expected outputs in new tests to use multiline string syntax

--- spec/commands/new_spec.rb
+++ spec/commands/new_spec.rb
@@ -37,7 +37,10 @@ describe 'New command' do
     allow(File).to receive(:open).with('loaders/testing.rb', 'w').and_yield(file_obj)
     expect(file_obj).to receive(:write).with(template).once
 
-    expected_output = "Creating #{'new'.green} loader\n"
+    expected_output = <<~TEXT
+      Creating #{'new'.green} loader
+    TEXT
+
     output = capture_stdout_from { attempt_command('new testing') }
     expect(output).to eq(expected_output)
   end
@@ -53,13 +56,19 @@ describe 'New command' do
     expect(File).to_not receive(:open).with('loaders/testing.rb')
     expect(File).to_not receive(:write).with('loaders/testing.rb')
 
-    expected_output = "Editing loader\n"
+    expected_output = <<~TEXT
+      Editing loader
+    TEXT
+
     output = capture_stdout_from { attempt_command('new testing') }
     expect(output).to eq(expected_output)
   end
 
   it 'outputs error message if loader name is not provided' do
-    expected_output = "Missing #1 positional argument: 'name'".red + "\n"
+    expected_output = <<~TEXT
+      #{"Missing #1 positional argument: 'name'".red}
+    TEXT
+
     output = capture_stdout_from { attempt_command('new') }
     expect(output).to eq(expected_output)
   end
@@ -75,7 +84,10 @@ describe 'New command' do
     expect(File).to_not receive(:open).with('loaders/testing2.rb')
     expect(File).to_not receive(:write).with('loaders/testing2.rb')
 
-    expected_output = "Editing loader\n"
+    expected_output = <<~TEXT
+      Editing loader
+    TEXT
+
     output = capture_stdout_from { attempt_command('new Testing2') }
 
     expect(output).to eq(expected_output)
@@ -83,21 +95,30 @@ describe 'New command' do
 
   context 'outputs error message if loader name is not valid because' do
     it 'uses kebab case' do
-      expected_output = "Invalid loader name 'im-bad-kebab-case'.".red + "\n"
+      expected_output = <<~TEXT
+        #{"Invalid loader name 'im-bad-kebab-case'.".red}
+      TEXT
+
       output = capture_stdout_from { attempt_command('new im-bad-kebab-case') }
 
       expect(output).to eq(expected_output)
     end
 
     it 'starts terms with numbers' do
-      expected_output = "Invalid loader name 'wrong_1'.".red + "\n"
+      expected_output = <<~TEXT
+        #{"Invalid loader name 'wrong_1'.".red}
+      TEXT
+
       output = capture_stdout_from { attempt_command('new wrong_1') }
 
       expect(output).to eq(expected_output)
     end
 
     it 'has invalid characters' do
-      expected_output = "Invalid loader name 'wrong!'.".red + "\n"
+      expected_output = <<~TEXT
+        #{"Invalid loader name 'wrong!'.".red}
+      TEXT
+
       output = capture_stdout_from { attempt_command('new wrong!') }
 
       expect(output).to eq(expected_output)
@@ -109,7 +130,11 @@ describe 'New command' do
     allow(File).to receive(:exist?).with('loaders/testing.rb').and_return(true)
     expect_any_instance_of(Commands::New).to_not receive(:system)
 
-    expected_output = "Editing loader\n#{"Could not open loader because default editor isn't set.".yellow}\n"
+    expected_output = <<~TEXT
+      Editing loader
+      #{"Could not open loader because default editor isn't set.".yellow}
+    TEXT
+
     output = capture_stdout_from { attempt_command('new testing') }
 
     expect(output).to eq(expected_output)
