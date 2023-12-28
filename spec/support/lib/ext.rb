c453da4ce4183ff4ff77eb9edb250bd3267c63ca
Hikari Luz <hikaridesuyoo@gmail.com>
2023-12-28 11:06:39 -0300

    fix booting test

--- spec/support/lib/ext.rb
+++ spec/support/lib/ext.rb
@@ -6,28 +6,25 @@ class IO
   end
 
   def read_until(string)
-    result = ''
+    Timeout.timeout(5) do
+      result = ''
+      end_index = 0
 
-    i = 0
-    stop_reading = lambda do |char|
-      match = string[i] == char
+      loop do
+        result_char = read_nonblock(1)
+        result << result_char
 
-      next true if i == 0 && !match
+        return result if end_index == (string.length - 1)
 
-      i += 1
-
-      match && i == string.length - 1
-    end
-
-    loop do
-      result_char = read_nonblock(1)
-      result << result_char
-
-      return result unless stop_reading.call(result_char)
-
-    rescue IO::WaitReadable
-      IO.select([self])
-      retry
+        if result_char == string[end_index]
+          end_index += 1
+        else
+          end_index = 0
+        end
+      rescue IO::WaitReadable
+        IO.select([self])
+        retry
+      end
     end
   end
 end
--- spec/system/booting_spec.rb
+++ spec/system/booting_spec.rb
@@ -4,6 +4,7 @@ describe 'Postwoman' do
   it 'boots up succesfully in sandbox mode' do
     PTY.spawn('postwoman') do |stdout, _, _|
       output = stdout.read_all
+      puts output
       expect(unstyled_stdout_from { print output }).to include("Type 'help' for more information")
       expect(unstyled_stdout_from { print output }).to include('sandbox> ')
     end
